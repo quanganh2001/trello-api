ebd4b1a2c347fd2a14a927c9293d506af2f9c781
quanganh2001 <cocoi5659@gmail.com>
2024-06-16 06:43:04 +0700

    50. Viết API với Express Router, hiểu HTTP Status Codes

--- src/routes/v1/boardRoutes.js
+++ src/routes/v1/boardRoutes.js
@@ -3,19 +3,17 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
-
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoutes'
 
 const Router = express.Router()
 
-/** Check APIs v1/status */
-Router.get('/status', (req, res) => {
-  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.' })
-})
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'GET: API get list boards' })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' })
+  })
 
-/** Board APIs */
-Router.use('/boards', boardRoutes)
-
-export const APIs_V1 = Router
+export const boardRoutes = Router
