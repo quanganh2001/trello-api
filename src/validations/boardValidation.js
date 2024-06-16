93836588d168cc7618614a8eeaf4bce302dc78ea
quanganh2001 <cocoi5659@gmail.com>
2024-06-16 20:21:43 +0700

    55. Môi trường Dev & Production trong dự án vì sao lại quan trọng?

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -5,7 +5,7 @@
  */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/apiError'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   /**
