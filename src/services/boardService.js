5cde08a0c0106afa14d184d94924fc706c9216c8
quanganh2001 <cocoi5659@gmail.com>
2024-06-20 05:50:42 +0700

    59. Tại sao nên check data ở cả Validation và Model Schema?

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -18,11 +18,9 @@ const createNew = async (reqBody) => {
 
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
     const createdBoard = await boardModel.createNew(newBoard)
-    console.log(createdBoard)
 
     // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
     const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
-    console.log(getNewBoard)
 
     // Làm thêm các xử lý logic khác với các Collection khác tùy đặc thù dự án...vv
     // Bản email, notification về cho admin khi có 1 cái board mới được tạo...vv
