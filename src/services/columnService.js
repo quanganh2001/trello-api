01b0a4782ec27e7f3d27444078b332a538e5642f
quanganh2001 <cocoi5659@gmail.com>
2024-07-30 11:20:15 +0700

    66. Viết 2 APIs tạo Column & Card trong ứng dụng Trello (phần 1)

---
+++ src/services/columnService.js
@@ -0,0 +1,26 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import { columnModel } from '~/models/columnModel'
+
+const createNew = async (reqBody) => {
+  try {
+    // Xử lý logic dữ liệu tùy đặc thù dự án
+    const newColumn = {
+      ...reqBody
+    }
+    const createdColumn = await columnModel.createNew(newColumn)
+    const getNewColumn = await columnModel.findOneById(createdColumn.insertedId)
+
+    // ...
+
+    return getNewColumn
+  } catch (error) { throw error }
+}
+
+export const columnService = {
+  createNew
+}
